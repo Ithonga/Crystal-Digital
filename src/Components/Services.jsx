7048445bc0327161f21665635497bb695b5c79fd
Ithonga <ithongadev@gmail.com>
2025-04-12 11:48:05 +0300

    modified the services section

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -29,15 +29,15 @@ function Services() {
   ];
 
   return (
-    <div className="flex items-center justify-center gap-8 h-screen w-full bg-blue-300">
+    <div className="flex items-center justify-center gap-8 px-9 md:px-20 py-[80px] h-screen w-full">
       {/* <div className="flex gap-8 p-9 bg-green-300 h-full"> */}
-        <img src="/bg.jpg" alt="" className="rounded-3xl object-cover w-[400px] h-[600px]" />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <img src="/bg.jpg" alt="" className="rounded-3xl object-cover w-[400px] h-full" />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 h-full w-full ">
           {services.map((item) => (
-            <div key={item.id} className="bg-red-500 rounded-3xl flex flex-col items-center justify-center w-[400px] h-[350px] ">
+            <div key={item.id} className=" shadow-2xl rounded-3xl flex flex-col items-center justify-center ">
                 <img src={item.icon} alt={item.name} className="h-12 "/>
               <h1>{item.name}</h1>
-              <p>{item.description}g</p>
+              <p>{item.description}</p>
             </div>
           ))}
         </div>
