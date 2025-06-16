34aa36ceb20c69a4ae92c30c3faff67b8b7e2f11
Ithonga <ithongadev@gmail.com>
2025-06-16 19:34:08 +0300

    added a loading state

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,42 @@ import Contact from "./pages/Contact";
 import Header from "./Components/Header";
 import TopHeader from "./Components/TopHeader";
 import Footer from "./Components/Footer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const validRoutes = ["/", "/about", "/services", "/contact"];
   const isValidRoute = validRoutes.includes(location.pathname); // Check if current path is valid
+  const [loading, setLoading] = useState("true");
 
-  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, [])
+
+  if (loading) {
+    return <div className="flex flex-col gap-3 font-bold text-2xl text-center items-center justify-center h-screen bg-[#00afef]">
+      <div className="animate-spin rounded-full h-20 w-20 border-b-4 border-white"></div>
+      {/* <div className="w-4 h-4 bg-green rounded-full animate-bounce"></div>
+      <div className="w-4 h-4 bg-white rounded-full animate-bounce "></div>
+      <div className="w-4 h-4 bg-white rounded-full animate-bounce [animation-delay:-0.2s]"></div>
+      <div className="w-4 h-4 bg-white rounded-full animate-bounce [animation-delay:-0.4s]"></div> */}
+      <p className="text-white text-[15px]">Loading...</p>
+    </div >
+  }
 
   return (
     <>
-        {isValidRoute && <TopHeader />}
-        {isValidRoute && <Header />}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Service />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        {isValidRoute && <Footer />}
+      {isValidRoute && <TopHeader />}
+      {isValidRoute && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {isValidRoute && <Footer />}
     </>
   );
 }
